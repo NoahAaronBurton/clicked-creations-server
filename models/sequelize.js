164c2f4592904f67620adb9fec0d46a923187f1c
Noah Burton <nburton115@gmail.com>
2024-02-07 11:50:48 -0700

    Update Sequelize configuration

--- models/sequelize.js
+++ models/sequelize.js
@@ -1,9 +1,13 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('clicked_creations', process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
+const sqlPort = process.env.DATABASE_PORT || 3306;
+const dbName = process.env.DATABASE_NAME || 'clicked_creations';
+
+const sequelize = new Sequelize(dbName, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
     host: process.env.DATABASE_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port: sqlPort
 });
 
 const User = sequelize.define('user', {
