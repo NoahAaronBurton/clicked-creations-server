4e4eb7ebf26d85b2ea1537e023924727b078a2b3
Noah Burton <nburton115@gmail.com>
2024-02-08 11:39:54 -0700

    Fix Google authentication err handling

--- passport.js
+++ passport.js
@@ -3,7 +3,6 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const passport = require('passport');
 const { User } = require('./models/sequelize');
 
-
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
@@ -11,27 +10,39 @@ passport.use(new GoogleStrategy({
     scope: ['profile', 'email']
 },
 function(accessToken, refreshToken, profile, done) {
-    // console.log(profile);
     User.findOrCreate({ 
         where: { googleId: profile.id },
         defaults: { name: profile.displayName }
     })
     .then(([user, created]) => {
+        if (!user) {
+            return done(new Error('No user found or created'), null);
+        }
         done(null, user);
     })
-    .catch(done);
+    .catch(err => {
+        done(err, null);
+    });
 }));
 
 passport.serializeUser((user, done) => {
+    if (!user) {
+        return done(new Error('No user to serialize'), null);
+    }
     done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
     User.findByPk(id)
     .then(user => {
+        if (!user) {
+            return done(new Error('No user found with this id'), null);
+        }
         done(null, user);
     })
-    .catch(err => done(err));
+    .catch(err => {
+        done(err, null);
+    });
 });
 
 module.exports = passport;
